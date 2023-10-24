0f8093f219df9e87908b98064dcc7155f782cd87
kaden-emrich <125313480+kaden-emrich@users.noreply.github.com>
2023-10-24 10:16:41 -0400

    started network.js

--- text-color/script.js
+++ text-color/script.js
@@ -2,12 +2,8 @@ var net = new brain.NeuralNetwork();
 
 const data = [
     {
-        input: { r: 0, g: 0, b: 0 },
-        output:[1]
-    },
-    {
         input: { r: 1, g: 1, b: 1 },
-        output: [0]
+        output:[0]
     }
 ];
 
