20b97ec4437d322867b8bebc933a0abcb9fa2531
Kaden Emrich <kadenemrich@gmail.com>
2023-10-24 13:29:43 -0400

    created neural network

--- network.js
+++ network.js
@@ -1,3 +1,21 @@
+class NeuralNetwork {
+    constructor(neuronCounts) {
+        this.levels = [];
+        for(let i = 0; i < neuronCounts.length; i++) {
+            this.levels.push(new Level(neuronCounts[i], neuronCounts[i+1]));
+        }
+    }// constructor(neuronCounts)
+
+    static feedForward(givenInputs, network) {
+        let outputs = Level.feedForward(givenInputs, network.levels[0]);
+        for(let i = 1; i < network.levels.length; i++) {
+            outputs = Level.feedForward(outputs, network.levels[i]);
+        }
+
+        return outputs;
+    }// static feedForward(givenInputs, network)
+}// class NeuralNetwork
+
 class Level {
     constructor(inputCount, outputCount) {
         this.inputs = new Array(inputCount);
