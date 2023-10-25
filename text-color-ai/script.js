efe2b2b80ede4c16e72acd25a70d5bd988dea3ba
kaden-emrich <125313480+kaden-emrich@users.noreply.github.com>
2023-10-25 10:11:25 -0400

    public release 1

--- text-color-ai/script.js
+++ text-color-ai/script.js
@@ -16,6 +16,7 @@ const blackButton = document.getElementById('black-button');
 const printButton = document.getElementById('print-button');
 const trainButton = document.getElementById('train-button');
 const testCaseNumDisplay = document.getElementById('test-cases-display');
+const statusDisplay = document.getElementById('status-display');
 let color;
 let guess;
 train();
@@ -41,17 +42,26 @@ function chooseColor(value) {
         input: color,
         output: [value]
     });
-    setRandomColor();
+
+    if(value != guess) {
+        train();
+    }
+    else {
+        setRandomColor();
+    }
     testCaseNumDisplay.innerText = data.length;
 }
 
-function train() {
+async function train() {
     colorEl.style.filter = 'brightness(0%)';
-    
-    net = trainNetwork(netStructure, 100, 1000, 0.2, data);
-    setRandomColor();
-    colorEl.style.filter = 'brightness(100%)';
-    console.log("trained");
+    updateStatus('training...')
+    setTimeout(() => {
+        net = trainNetwork(netStructure, 100, 1000, 0.2, data);
+        setRandomColor();
+        colorEl.style.filter = 'brightness(100%)';
+        console.log("trained");
+        updateStatus('none');
+    }, 10);
 }
 
 function print() {
@@ -66,3 +76,7 @@ function setRandomColor() {
     colorEl.style.background = `rgba(${color[0] * 255}, ${color[1] * 255}, ${color[2] * 255})`;
 }
 
+async function updateStatus(value, callback) {
+    statusDisplay.innerText = value;
+    if(callback && typeof callback === 'function') awaitcallback();
+}
